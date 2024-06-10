ccfb15a9ef09d4dd30affb808ee0905b9a98c553
ifahimkhan <khan.fahim95@hotmail.com>
2024-06-11 00:53:38 +0530

    Added sound effect and Animation of dice rolling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.androidx.animation)
+    implementation (libs.kotlinx.coroutines.core)
+    implementation (libs.kotlinx.coroutines.android)
 }
--- app/src/main/java/com/fahim/diceroller/MainActivity.kt
+++ app/src/main/java/com/fahim/diceroller/MainActivity.kt
@@ -1,22 +1,40 @@
 package com.fahim.diceroller
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.InfiniteRepeatableSpec
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.fahim.diceroller.ui.theme.DiceRollerTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,7 +51,11 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun DiceRollerApp() {
-    DiceWithButtonAndImage()
+    DiceWithButtonAndImage(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    )
 
 }
 
@@ -41,6 +63,8 @@ fun DiceRollerApp() {
 fun DiceWithButtonAndImage(
     modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
+
     var result by remember { mutableStateOf(1) }
     val imageResource = when (result) {
         1 -> R.drawable.dice_1
@@ -50,11 +74,78 @@ fun DiceWithButtonAndImage(
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
+    var isRolling by remember {
+        mutableStateOf(false)
+    }
+    val coroutineScope = rememberCoroutineScope()
+    val rotation = remember { Animatable(0f) }
+    val scale = remember { Animatable(1f) }
+    val mediaPlayer = remember {
+        MediaPlayer.create(context, R.raw.dice_roll)
+    }
+
+    LaunchedEffect(key1 = isRolling) {
+        if (isRolling) {
+            coroutineScope.launch {
+                launch {
+                    try {
+                        mediaPlayer.start()
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+                launch {
+                    rotation.animateTo(
+                        targetValue = 1000f,
+                        animationSpec = infiniteRepeatable(
+                            animation = tween(durationMillis = 1000, easing = LinearEasing),
+                            repeatMode = RepeatMode.Restart
+                        )
+                    )
+                }
+                launch {
+                    scale.animateTo(
+                        targetValue = 1.2f,
+                        animationSpec = InfiniteRepeatableSpec(
+                            animation = tween(durationMillis = 500, easing = FastOutSlowInEasing),
+                            repeatMode = RepeatMode.Reverse
+                        )
+                    )
+                }
+            }
+        } else {
+            rotation.snapTo(0f)
+            scale.snapTo(1f)
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.stop()
+                mediaPlayer.prepare()
+            }
+        }
+    }
+    DisposableEffect(Unit) {
+        onDispose {
+            mediaPlayer.release()
+        }
+    }
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
         Image(
-            modifier = Modifier.clickable {
-                result = (1..6).random()
-            },
+            modifier = Modifier
+                .graphicsLayer {
+                    rotationZ = rotation.value
+                    scaleX = scale.value
+                    scaleY = scale.value
+                }
+                .clickable(indication = null, // Disable the visual feedback
+                    interactionSource = remember { MutableInteractionSource() })
+                {
+                    if (isRolling) return@clickable // Prevent rolling when already rolling
+                    isRolling = true
+                    coroutineScope.launch {
+                        delay(1000)  // Duration of the rolling animation
+                        result = (1..6).random()
+                        isRolling = false
+                    }
+                },
             painter = painterResource(imageResource),
             contentDescription = result.toString()
         )
